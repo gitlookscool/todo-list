7a2860d94ae3d0a6715720b16090a57e79376039
gitlookscool <surajdsrv.gmail.com>
2022-09-15 04:57:13 +0200

    testing commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef, useEffect } from 'react'
+import TodoList from './TodoList';
+import './app.css'
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
+  
+  
+  const [todos, setTodos] = useState([])
+  const todoNameRef = useRef()
+  const LOCAL_STORAGE_KEY = 'todos.todoapp'
+
+  function toggleTodo(id){
+    const newTodos = [...todos]
+    const todo = newTodos.find(todo=> todo.id===id)
+    todo.complete = !todo.complete
+    setTodos(newTodos)
+  }
+
+  useEffect(()=>{
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+    if(storedTodos) return setTodos(storedTodos)
+  },[])
+
+  useEffect(()=>{
+    localStorage.setItem(LOCAL_STORAGE_KEY,JSON.stringify(todos))
+  },[todos])
+
+  function handleRemoveTodo(){
+    const newTodos = todos.filter(todo => todo=!todo.complete)
+    setTodos(newTodos)
+  }
+
+  function handleAddTodo(e){
+    const name = todoNameRef.current.value;
+    if (name === '') return
+
+    setTodos(prevTodos => {
+      return [...prevTodos,{id: uuidv4(), name:name, complete: false}]
+    })
+    
+    todoNameRef.current.value = null;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='todo-list'>
+      <p>Testing Git commit</p>
+      <h1>Todo List</h1>
+      <input 
+      ref={todoNameRef}
+      type='text'
+      placeholder='add todo'
+      />
+      <button onClick={handleAddTodo}>Add Todo</button>
+      <button onClick={handleRemoveTodo}>Remove Todo</button>
+      <TodoList todos={todos} toggleTodo={toggleTodo} />
     </div>
-  );
+  )
 }
 
 export default App;
